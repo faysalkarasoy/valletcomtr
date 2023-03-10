533cfa90c18f26cc4ce6ab0359985ba67fd8ac24
faysalkarasoy <77213914+faysalkarasoy@users.noreply.github.com>
2023-03-10 15:39:35 +0300

    Add files via upload

---
+++ PostData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ValletComTR_Ornek
+{
+    public struct PostData
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string shopCode { get; set; }
+        /// <summary>
+        /// Maks 200 karakter
+        /// </summary>
+        public string productName { get; set; }
+        public List<ProductData> productData { get; set; }
+
+
+        /// <summary>
+        /// FIZIKSEL_URUN  veya   DIJITAL_URUN
+        /// </summary>
+        public string productType { get; set; }
+
+        /// <summary>
+        /// Siparişe ait sepetteki ürünlerin toplam tutarı.
+        /// </summary>
+        public decimal productsTotalPrice { get; set; }
+
+        /// <summary>
+        /// Api üzerinden geçecek nihai tutar. Taksit oranları bu tutar üzerine eklenerek hesaplanır.
+        /// </summary>
+        public decimal orderPrice { get; set; }
+
+        /// <summary>
+        /// TRY,EUR,USD
+        /// </summary>
+        public string currency { get; set; }
+
+        /// <summary>
+        /// Maks 50 karakter, İlgili siparişinizin sizin sisteminiz tarafındaki sipariş ID yada Sipariş Kodu. Ödenmemiş bir sipariş yada benzersiz olmalıdır.
+        /// </summary>
+        public string orderId { get; set; }
+
+        /// <summary>
+        /// tr,en,de,ru
+        /// </summary>
+        public string locale { get; set; }
+
+        /// <summary>
+        /// Maks 200 karakter, İstekte gönderilirse response olarak size geri döndürülür. Request/response eşlemesi yapmak için kullanılır.
+        /// </summary>
+        public string conversationId { get; set; }
+
+        /// <summary>
+        /// Sipariş Sahibi Adı
+        /// </summary>
+        public string buyerName { get; set; }
+
+        /// <summary>
+        /// Şipariş Sahibi Soyadı
+        /// </summary>
+        public string buyerSurName { get; set; }
+
+        /// <summary>
+        /// Sipariş Sahibi Telefon Numarası Uyarı: Telefon yanlış olan siparişler iptal edilir. Güvenlik doğrulaması için müşteriye ulaşılması gerekebilir.
+        /// </summary>
+        public string buyerGsmNo { get; set; }
+        public string buyerIp { get; set; }
+
+        /// <summary>
+        /// Maks 80
+        /// </summary>
+        public string buyerMail { get; set; }
+
+        /// <summary>
+        /// Maks 200
+        /// </summary>
+        public string buyerAdress { get; set; }
+
+        /// <summary>
+        /// Müşteri ÜLKE, Maks 70
+        /// </summary>
+        public string buyerCountry { get; set; }
+
+        /// <summary>
+        /// Müşteri Şehir, Maks 70
+        /// </summary>
+        public string buyerCity { get; set; }
+
+        /// <summary>
+        /// Müşteri İlçe Maks 70
+        /// </summary>
+        public string buyerDistrict { get; set; }
+
+        /// <summary>
+        /// Müşterinin başarılı işlemde yönlendirileceği sayfa
+        /// </summary>
+        public string callbackOkUrl { get; set; }
+
+        /// <summary>
+        /// Müşterinin başarısız işlemde yönlendirileceği sayfa
+        /// </summary>
+        public string callbackFailUrl { get; set; }
+
+        public static string module = "NATIVE_NET";
+
+        /// <summary>
+        /// bu değeri boş gönderin
+        /// </summary>
+        public string hash { get; set; }
+
+    }
+}
---
+++ ValletRequest.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ValletComTR_Ornek
+{
+    public class ValletRequest
+    {
+        const string Api_URL = "https://www.vallet.com.tr/api/v1/create-payment-link";
+
+        private ValletConfig _valletConfig;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valletConfig"></param>
+        public ValletRequest(ValletConfig valletConfig)
+        {
+           _valletConfig = valletConfig;
+        }
+
+        private string Hash_generate(string text)
+        {
+            string source = text;
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(_valletConfig.userName + _valletConfig.password + _valletConfig.shopCode + text + _valletConfig.hash);
+                byte[] hashBytes = sha1Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                source = Convert.ToBase64String(Encoding.UTF8.GetBytes(hash));
+            }
+            return source;
+        }
+
+        public object Create_payment_link(PostData post_data)
+        {
+            post_data.userName = _valletConfig.userName;
+            post_data.password = _valletConfig.password;
+            post_data.shopCode = _valletConfig.shopCode;
+
+            post_data.hash = Hash_generate(post_data.orderId + post_data.currency + post_data.orderPrice + post_data.productsTotalPrice + post_data.productType + post_data.callbackOkUrl + post_data.callbackFailUrl);
+
+            var Response = Send_post(post_data);
+
+            return Response;
+        }
+
+        private object Send_post(PostData post_data)
+        {
+
+            object Result;
+            object StatusCode;
+
+            var string_data = JsonConvert.SerializeObject(post_data);
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(Api_URL);
+                request.Accept = "application/json";
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                using (StreamWriter sw = new StreamWriter(request.GetRequestStream()))
+                {
+                    sw.Write(string_data);
+                }
+
+                var response = (HttpWebResponse)request.GetResponse();
+                StatusCode = response.StatusCode;
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    Result = sr.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                using (WebResponse response = ex.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+                    StatusCode = httpResponse.StatusCode;
+
+                    using (Stream data = response.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(data))
+                        {
+                            Result = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return Result;
+        }
+    }
+
+}
